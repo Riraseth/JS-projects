d29abb0a6186d3563aa27de85901152739167d49
Łukasz Zabuski <riraseth@gmail.com>
2020-03-22 20:09:15 +0100

    https update

--- weather_app/scripts/forecast.js
+++ weather_app/scripts/forecast.js
@@ -2,30 +2,23 @@ const key = 'q9B7o7tHUlpl6bSAmloZNC4b7o3P3v0D';
 
 //get weather info
 
-const getWeather = async (locationKey) => {
+const getWeather = async locationKey => {
+  const base = 'https://dataservice.accuweather.com/currentconditions/v1/';
+  const query = `${locationKey}?apikey=${key}`;
+  const response = await fetch(base + query);
 
-    const base = 'http://dataservice.accuweather.com/currentconditions/v1/'
-    const query = `${locationKey}?apikey=${key}`;
-    const response = await fetch(base + query);
-
-    const data = await response.json();
-    return data[0];
+  const data = await response.json();
+  return data[0];
 };
-
 
 //get city info
-const getCity = async (city) => {
-
-    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
-    const query = `?apikey=${key}&q=${city}`; //dodajemy zapytanie przez '?'
-    const response = await fetch(base + query);
-    const data = await response.json();
-    return (data[0]);
+const getCity = async city => {
+  const base = 'https://dataservice.accuweather.com/locations/v1/cities/search';
+  const query = `?apikey=${key}&q=${city}`; //dodajemy zapytanie przez '?'
+  const response = await fetch(base + query);
+  const data = await response.json();
+  return data[0];
 };
-
-
-
-
 
 // getCity('Siedlce').then(data => { //ta data przekazywane jest z getcity
 //     return getWeather(data.Key);
